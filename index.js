0e321fde3086d3322be43e84ad6fd5d55989f639
Vap0r1ze <superdash993@gmail.com>
2022-08-24 06:15:52 -0400

    add lookbehind and lookahead

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const { Plugin } = require('powercord/entities');
 const { getModule } = require('powercord/webpack');
 
 const Color = require('./components/Color');
-const COLOR_PATTERN = /^((?:#|0x)(?:[a-f0-9]{8}|[a-f0-9]{6}|[a-f0-9]{3})|(?:rgb|hsl)a?\([^\)]*?\))/i;
+
+const LOOKBEHIND_PATTERN = /\W$/;
+const COLOR_PATTERN = /^((?:#|0x)(?:[a-f0-9]{8}|[a-f0-9]{6}|[a-f0-9]{3})|(?:rgb|hsl)a?\([^\)]*?\))(?!\w)/i;
 
 module.exports = class ChatComponents extends Plugin {
     async startPlugin() {
@@ -12,8 +14,13 @@ module.exports = class ChatComponents extends Plugin {
 
         // Add rule (See: https://github.com/Khan/simple-markdown#extension-overview)
         parser.defaultRules.chatComponentsColor = {
-            order: parser.defaultRules.mention.order,
-            match: source => COLOR_PATTERN.exec(source),
+            order: parser.defaultRules.text.order - 1,
+            match: (source, state) => {
+                if (state.prevCapture && !LOOKBEHIND_PATTERN.test(state.prevCapture)) {
+                    return null;
+                }
+                return COLOR_PATTERN.exec(source);
+            },
             parse: match => {
                 return {
                     color: match[1],
