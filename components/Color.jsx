e900f63a14c27f3f87553e988988226016228782
Vap0r1ze <superdash993@gmail.com>
2022-08-24 02:44:04 -0400

    add markdown rule instead of injecting parser

--- components/Color.jsx
+++ components/Color.jsx
@@ -4,38 +4,27 @@ const { _parseColor, _numberToTextColor } = require('../utils');
 const ColorPopout = require('./ColorPopout');
 const Popout = getModuleByDisplayName('Popout', false);
 
-class StringPart extends React.PureComponent {
-	render() {
-		const { parts } = this.props;
-
-		for (let i = 1; i < parts.length; i += 2) {
-			let color = parts[i];
-			if (typeof color !== 'string') continue;
-
-			parts[i] = <Popout
-				position="top"
-				align="center"
-				animation="1"
-                renderPopout={() => React.createElement(ColorPopout, {color})}
+function Color(color) {
+	return <Popout
+		position="top"
+		align="center"
+		animation="1"
+		renderPopout={() => React.createElement(ColorPopout, { color })}
+	>
+		{props =>
+			<span
+				{...props}
+				className='chatcomps-color-text'
+				style={{
+					'--color': color,
+					'--hoveredColor': _numberToTextColor(color),
+					'--backgroundColor': _parseColor(color, 0.1)[4]
+				}}
 			>
-				{props => 
-					<span
-						{...props}
-						className='chatcomps-color-text'
-						style={{
-							'--color': color,
-                            '--hoveredColor': _numberToTextColor(color),
-                            '--backgroundColor': _parseColor(color, 0.1)[4]
-						}}
-					>
-						{color}
-					</span>
-				}
-			</Popout>
+				{color}
+			</span>
 		}
-
-		return parts;
-	}
+	</Popout>
 }
 
-module.exports = StringPart;
+module.exports = Color;
--- index.js
+++ index.js
@@ -1,58 +1,38 @@
 const { Plugin } = require('powercord/entities');
-const { React, getModule } = require('powercord/webpack');
-const { inject, uninject } = require('powercord/injector');
+const { getModule } = require('powercord/webpack');
 
 const Color = require('./components/Color');
+const COLOR_PATTERN = /^((?:#|0x)(?:[a-f0-9]{8}|[a-f0-9]{6}|[a-f0-9]{3})|(?:rgb|hsl)a?\([^\)]*?\))/i;
 
 module.exports = class ChatComponents extends Plugin {
     async startPlugin() {
         this.loadStylesheet('styles.css');
 
-		const parser = await getModule(['parse', 'parseTopic']);
-        
-		inject('chatcomponents', parser, 'parse', (args, res = {}) => { // *steals asportnoy's tone-indicator*
-    		if (!Array.isArray(res)) return res;
-            
-            // Loop through each part of the message
-            return res.map(el => {
-                if (typeof el !== 'string') {
-                    if (el?.props?.parts) { // face tone-indicators
-                        return el; //todo
-                    }
+        const parser = this.parser = await getModule(['parse', 'parseTopic']);
 
-                    return el;
+        // Add rule (See: https://github.com/Khan/simple-markdown#extension-overview)
+        parser.defaultRules.chatComponentsColor = {
+            order: parser.defaultRules.mention.order,
+            match: source => COLOR_PATTERN.exec(source),
+            parse: match => {
+                return {
+                    color: match[1],
+                    content: match[0]
                 }
+            },
+            react: node => Color(node.color),
+        };
 
-                const colors = el.split(/(?<!\w)((?:#|0x)(?:[a-f0-9]{3}|[a-f0-9]{6}|[a-f0-9]{8})|(?:rgb|hsl)a?\(.*?\))(?!\w)/gi);
-
-                if (!colors) return el;
-
-                const res = [];
-                let error = false;
-
-                for (const [i, color] of colors.entries()) {
-                    if (typeof color !== 'string') continue;
-
-                    if (i % 2 === 0) {
-                        if (error) res[res.length - 1] += color;
-                        else res.push(color);
-                    } else if (color !== null) {
-                        res.push(color);
-                        error = false;
-                    } else {
-                        res[res.length - 1] += color;
-                        error = true;
-                    }
-                }
-
-                return React.createElement(StringPart, {
-                    parts: res,
-                });
-            });
-        });
+        this.refreshParser();
     }
 
     pluginWillUnload() {
-        uninject('chatcomponents');
+        delete this.parser.defaultRules.chatComponentsColor;
+        this.refreshParser();
+    }
+
+    refreshParser() {
+        // Recreate parser with new rules
+        this.parser.parse = this.parser.reactParserFor(this.parser.defaultRules);
     }
 }
